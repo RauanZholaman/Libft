0cdc858db214031b1fbbe512eb06f3a30d4d49cd
rauan <rauan@DESKTOP-D1DNDI9>
2025-05-07 18:08:17 +0800

    Add isdigit function

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,30 +1,29 @@
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
 	return (0);
 }
-
+/*
 #include <stdio.h>
 #include <ctype.h>
 
 int	main()
 {
-	char c = ((int)9);
+	unsigned char d = '9';
 
-	int d = 0;
+	int c = (unsigned char)d;
+
 	printf("Original Function. Input(1): %d \n", ft_isdigit(1));
 	printf("In-built Function. Input(1): %d \n", isdigit(1));
 
-	printf("In-built Function. Input('1'): %d \n", ft_isdigit('1'));
-	printf("In-built Function. Input('1'): %d \n", isdigit('1'));
+	printf("\nOriginal Function. Input('1'): %d \n", ft_isdigit(c));
+	printf("In-built Function. Input('1'): %d \n", isdigit(c));
 
 	printf("\nOriginal Function. Input(-1): %d \n", ft_isdigit(-1));
 	printf("In-built Function. Input(-1): %d \n", isdigit(-1));
-
 	
 	printf("\nOrigin  Function. Input('a'); %d \n", ft_isdigit('a'));
 	printf("In-built Function. Input('a');  %d \n", isdigit('a'));
 }
+*/
