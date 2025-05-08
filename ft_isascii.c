157b600550aac08f92f5e946212c738d4469a555
rauan <rauan@DESKTOP-D1DNDI9>
2025-05-08 12:11:17 +0800

    Implement isascii function

---
+++ ft_isascii.c
@@ -0,0 +1,15 @@
+int	ft_isascii(int d)
+{
+	if (d >= 0 && d <= 127)
+		return (1);
+	return(0);	
+}
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+	printf("My function: %d\n", ft_isascii('='));
+	printf("Original Function: %d\n", isascii('='));
+}
