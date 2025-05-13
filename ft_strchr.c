cfa16fa7da9fe98c3520e26685363918adabbd4d
rauan <rauan@DESKTOP-D1DNDI9>
2025-05-13 15:48:21 +0800

    Implement the strchr function

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,20 +1,27 @@
-/*
 char	*ft_strchr(const char *str, int c)
 {
+	while (*str != '\0')
+	{
+		if (*str == c)
+			return ((char *)str);
+		str++;
+	}
 
+	return (0);
 }
-*/
 
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-	char s[] = "Hello World!";
+	char s[] = "Jerrp,";
 
-	char c = 'H';
+	char c = '#';
 
 	char *p = strchr(s, c);
+	char *d = ft_strchr(s, c);
 
-	printf("String given is: (%s), and the char: (%c). The result is: %s.\n", s, c, p); 
+	printf("String given is: (%s), and the char: (%c). strchr(): %s.\n", s, c, p); 
+	printf("String given is: (%s), and the char: (%c). ft_strchr(): %s.\n", s, c, d); 
 }
