96804324e3eaea947d0dcdaf1458d7b2410390ea
Avalon <rauankairav@gmail.com>
2025-06-02 12:20:55 +0800

    Polish strnstr function

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,25 +1,77 @@
 #include <stddef.h>
 
-char *ft_strnstr(const char *s1, const char *s2, size_t len)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (i < len && *s1 != '\0')
+	while (i < n && (*s1 != '\0' || *s2 != '\0'))
 	{
-		if (*s1 != *s2)
-			return s2;
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		i++;
+		s1++;
+		s2++;
+	}
+	
+	return (0);
+}
+
+int		ft_strlen(const char *str)
+{
+	int		i;
+	
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+char *ft_strnstr(const char *s1, const char *s2, size_t len)
+{
+	size_t		i;
+	size_t		s2_size;
+
+	i = 0;
+	s2_size = ft_strlen(s2);
+	if (len < s2_size)
+		return (0);
+	if (*s2 == '\0')
+	{
+		return (char *)s1;
+	}
+	else
+	{
+		while (i <= len)
+		{
+			if (ft_strncmp(s1 + i, s2, s2_size) == 0)
+				return (char *)s1 + i;
+			i++;
+		}	
 	}
 	return (0);
 }
 
-#include <string.h>
 #include <stdio.h>
 
 int main()
 {
-	char w1[] = "Hello";
-	char w2[] = "Hello";
+	char w1[] = "Hello!";
+	char w2[] = "Hel";
 
-	printf("Original strnstr function: %d", ft_strnstr(w1, w2, 3));
+	printf("My ft_strnstr function: %s", ft_strnstr(w1, w2, 5));
+
+	printf("\n\nEmpty needle: %s\n", ft_strnstr("hello", "", 5)); 	// Should return "hello"
+    printf("Empty haystack: %s\n", ft_strnstr("", "abc", 3)); 		// NULL
+    printf("Zero len: %s\n", ft_strnstr("hello", "he", 0)); 		// NULL
+    printf("Len < needle: %s\n", ft_strnstr("hello", "hello", 4)); 	// NULL
+    printf("Len > haystack: %s\n", ft_strnstr("hi", "i", 5)); 		// Pointer to "i"
+    printf("No match: %s\n", ft_strnstr("hello", "xyz", 5)); 		// NULL
+    printf("Partial match: %s\n", ft_strnstr("hell", "hello", 4)); 	// NULL
+    printf("Match at end: %s\n", ft_strnstr("hello", "lo", 5)); 	// Pointer to "lo"
+    printf("Case mismatch: %s\n", ft_strnstr("Hello", "hello", 5)); // NULL
+
 }
