26f7c596f821a9a8ccf8ba528446703798f59fbd
rauan <rauan@DESKTOP-D1DNDI9.localdomain>
2025-06-13 15:25:56 +0800

    Add new funtion declarations

--- libft.h
+++ libft.h
@@ -1,6 +1,14 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
+#include <stddef.h>
+
 int	ft_isdigit(int c);
+int     ft_isalpha(unsigned char c);
+int     ft_isalnum(int d);
+int     ft_isprint(int d);
+int     ft_isascii(int d);
+size_t  ft_strlen(const char *s);
+void	*ft_memcpy(void *dst_str, const void *src_str, size_t n);
 
 #endif
