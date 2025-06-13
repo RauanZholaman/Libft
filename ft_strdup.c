8cf016803a97f9133a9029e61727b5772d0e7309
rauan <rauan@DESKTOP-D1DNDI9.localdomain>
2025-06-13 21:13:00 +0800

    Add ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	size_t len;
+	char *s;
+
+	len = ft_strlen(str);
+	s = malloc(len + 1);
+
+	if (!s)
+		return (NULL);
+	ft_memcpy(s, str, len + 1);
+	
+	return (s);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *str = "Ты пидр.";
+	char *dup;
+	char *test;
+
+	dup = strdup(str);
+	test = ft_strdup(str);
+
+	printf("%s\n", dup);
+	printf("%s\n", test);
+}*/
