b98d3ecbf5f385b575a439416b56369f14339bab
Rauan Zholaman <rzholama@c2r6s10.42singapore.sg>
2025-06-14 21:51:51 +0800

    Add new SRC files, new commands for archive and executor

--- libft.h
+++ libft.h
@@ -10,7 +10,25 @@ int     ft_isalnum(int d);
 int     ft_isprint(int d);
 int     ft_isascii(int d);
 size_t  ft_strlen(const char *s);
-void	*ft_memcpy(void *dst_str, const void *src_str, size_t n);
 void	ft_bzero(void *s, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *str, int c);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+void	*ft_memcpy(void *dst_str, const void *src_str, size_t n);
+int	ft_memcmp(const void *str1, const void *str2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+char	*ft_strdup(const char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char *ft_strnstr(const char *s1, const char *s2, size_t len);
+int	ft_atoi(const char *str);
+void	*ft_calloc(size_t nitems, size_t size);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int	ft_tolower(int d);
+int	ft_toupper(int d);
+
+
 
 #endif
