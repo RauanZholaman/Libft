d03643563f1302d2728521006c02e995802d98db
rauan <rauan@DESKTOP-D1DNDI9.localdomain>
2025-06-16 01:36:43 +0800

    Changed trimming condition

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,33 +13,30 @@ int     is_inset(char c, char const *set)
 
 char    *ft_strtrim(char const *s1, char const *set)
 {
-    char    *str;
-    size_t  str_s1;
-    size_t  end;
+    char	*str;
+    size_t	end;
+    size_t	full_size;
     
     while (is_inset(*s1, set))
-    {
-        s1++;
-    }
+    	s1++;
     end = ft_strlen(s1) - 1;
     while (end > 0 && is_inset(s1[end], set))
-    {
         end--;
-    }
-    str_s1 = ft_strlen(s1);
-    str = malloc(str_s1 + 1);
+
+    full_size = end + 1;
+  
+    str = malloc(full_size + 1);
     if (!str)
         return (NULL);
 
     char *result = str;
-    while (*s1 != '\0' && str_s1--)
-    {
-        *str++ = *s1++;
-    }
+    size_t i = 0;
+    while (i < full_size)
+        *str++ = s1[i++];
     *str = '\0';
     return (result);
 }
-
+/*
 #include <stdio.h>
 
 int main()
@@ -48,4 +45,4 @@ int main()
     char *set = "xxx";
     char *new = ft_strtrim(s1, set);
     printf("%s\n", new);
-}
+}*/
