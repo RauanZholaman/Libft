e6a0679ab29f2ea8b8bb0a0eb1f7a8d2cda2d5ca
Rauan Zholaman <rzholama@c2r6s10.42singapore.sg>
2025-06-17 03:46:48 +0800

    Refactor function, handle edge case when the full string match the set

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,35 +14,33 @@ static int     is_inset(char c, char const *set)
 char    *ft_strtrim(char const *s1, char const *set)
 {
     char	*str;
+	size_t	start;
     size_t	end;
-    size_t	full_size;
-    char    *result;
     size_t  i;
-	
+
     if (!s1 || !set)
-	    return (NULL);    
-    while (is_inset(*s1, set))
-    	s1++;
-    end = ft_strlen(s1) - 1;
-    while (end > 0 && is_inset(s1[end], set))
+	    return (NULL);
+	start = 0;
+    while (s1[start] && is_inset(s1[start], set))
+		start++;
+    end = ft_strlen(s1);
+    while (end > start && is_inset(s1[end - 1], set))
         end--;
-    full_size = end + 1;
-    str = malloc(full_size + 1);
+    str = malloc(sizeof(char) * (end - start) + 1);
     if (!str)
         return (NULL);
-    result = str;
     i = 0;
-    while (i < full_size)
-        *str++ = s1[i++];
-    *str = '\0';
-    return (result);
+    while (start < end)
+        str[i++] = s1[start++];
+    str[i] = '\0';
+    return (str);
 }
 
 // #include <stdio.h>
 
 // int main()
 // {
-//     char *s1 = "xxxxxelloxxx";
+//     char *s1 = "xxxxxxxx";
 //     char *set = "xxx";
 //     char *new = ft_strtrim(s1, set);
 //     printf("%s\n", new);
